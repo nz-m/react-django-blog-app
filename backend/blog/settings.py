065895473b79531e9f885329ce958218a0515fba
Neaz Mahmud <neaz6160@gmail.com>
2022-10-23 23:40:48 +0600

    Update token lifetime

--- backend/blog/settings.py
+++ backend/blog/settings.py
@@ -57,7 +57,7 @@ REST_FRAMEWORK = {
 AUTH_USER_MODEL = 'api.Profile'
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=30),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=90),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
